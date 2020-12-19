d87076fc3b5c6246e6cc932d1163b5a4676b3dba
Alex Shtilman <gungam@outlook.com>
2020-12-20 00:18:55 +0200

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Perform the queries for both SQL and MongoDB for the following tasks
+
+1. Update table marks such that all marks for those who don’t have “Java Technologies” mark will be decreased on 20
+1. Update subject name ‘Java’ to ‘Java core’
+1. Insert new “Java Advanced” marks for all students. These marks should be equaled to an average mark among all subjects related to Java of each student. For example if a student has 100 of “Java Technologies” and 70 of “Java core” the “Java Advanced” mark should be 85 for the student. Hint: applying either “like” or regular expressionDisplay two best students of Front-End (React, JS)
+1. Delete mark objects of React of students who have mark of JS less than 80
+1. Delete students having average mark less than the college average mark
---
+++ sql/script.sql
@@ -0,0 +1,82 @@
+-- Update table marks such that all marks for those who don’t have “Java Technologies” mark will be decreased on 20
+CREATE TEMPORARY TABLE m2
+SELECT stid
+FROM marks
+WHERE suid = (
+        SELECT suid
+        from subjects
+        WHERE subject = "Java Technologies"
+    );
+UPDATE marks
+SET mark = mark - 20
+WHERE stid not in (
+        SELECT stid
+        from m2
+    );
+-- average mark
+UPDATE marks
+SET mark = mark - 20
+WHERE stid NOT IN (
+        SELECT stid
+        FROM students_marks_subjects
+        WHERE subject = 'Java Technologies'
+    );
+-- Update subject name ‘Java’ to ‘Java core’
+UPDATE subjects
+set subject = 'Java core'
+where subject = 'Java'
+    /*
+     Insert new “Java Advanced” marks for all students. 
+     These marks should be equaled to an average mark among all subjects related to Java of each student. 
+     For example if a student has 100 of “Java Technologies” and 70 of “Java core” the “Java Advanced” 
+     mark should be 85 for the student. Hint: applying either “like” or regular expressionDisplay 
+     two best students of Front-End (React, JS)*/
+INSERT INTO subjects
+VALUES (5, 'Java Advanced');
+INSERT INTO marks (stid, mark, suid)
+SELECT st.stid,
+    ROUND(AVG(m.mark)),
+    (
+        SELECT suid
+        from subjects
+        WHERE subject = 'Java Advanced'
+    )
+FROM marks m
+    JOIN subjects su on m.suid = su.suid
+    JOIN students st on m.stid = st.stid
+WHERE su.subject LIKE '%Java%'
+GROUP by st.stid -- Delete mark objects of React of students who have mark of JS less than 80
+DELETE FROM marks
+WHERE stid IN (
+        SELECT *
+        FROM (
+                SELECT stid
+                from marks m1
+                where EXISTS (
+                        SELECT *
+                        from marks m2
+                        where m1.stid = m2.stid
+                            and suid = (
+                                SELECT suid
+                                from subjects
+                                where subject = 'JS'
+                            )
+                            and m2.mark < 80
+                    )
+            ) AS p
+    )
+    AND suid = (
+        SELECT suid
+        from subjects
+        where subject = 'React'
+    ) -- Delete students having average mark less than the college average mark 
+DELETE FROM students
+WHERE stid IN (
+        SELECT stid
+        from marks
+        group by stid
+        HAVING AVG(mark) <(
+                SELECT AVG(mark)
+                FROM marks
+            )
+    )
